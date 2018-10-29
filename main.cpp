699e97e0b4d37b8035877da530b3e7a5af07ab45
José Antonio Hernández <pepehdezx2@gmail.com>
2018-10-29 12:52:16 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  linkedlist2
+//
+//  Created by Pepe Hdez hdez on 8/28/18.
+//  Copyright © 2018 Pepe Hdez hdez. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    miLista.addLast("Kevin");
+    miLista.addFirst("Llega");
+    
+    return 0;
+}
